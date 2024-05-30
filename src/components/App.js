fb088de1d3d06849e631c07db8ae540dd41396e1
Salah Hossam <salah3hossam@gmail.com>
2024-05-30 22:37:32 +0300

    v2

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Loader from "./components/Loader";
-import Error from "./components/Error";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
 import { useEffect, useReducer } from "react";
-import StartScreen from "./components/StartScreen";
-import Question from "./components/Question";
-import NextButton from "./components/NextButton";
-import Progress from "./components/Progress";
-import FinishScreen from "./components/FinishScreen";
-import Footer from "./components/Footer";
-import Timer from "./components/Timer";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
 
 const SECS_PER_QUESTION = 30;
 
